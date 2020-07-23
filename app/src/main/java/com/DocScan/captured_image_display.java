02d761de03641a6fc18a028756d5d7029dc061a3
Amoghavarsha <kgamoghavarsha@gmail.com>
2020-07-23 21:59:07 +0530

    2st commit -Added new activity and its java files activites: activity_captured_image_display.xml, activity_cropper.xml, activity_image_capture.xml, activity_cropper.xml, bottomsheet_for_ocr_text.xml, single_file_detail_shower.xml, image_display.xml

---
+++ app/src/main/java/com/DocScan/captured_image_display.java
@@ -0,0 +1,14 @@
+package com.DocScan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class captured_image_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_captured_image_display);
+    }
+}
---
+++ app/src/main/java/com/DocScan/image_capture.java
@@ -0,0 +1,14 @@
+package com.DocScan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class image_capture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_capture);
+    }
+}
